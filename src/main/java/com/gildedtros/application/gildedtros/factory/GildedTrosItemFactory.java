94cb07c187a348936bd4bb12dbc66a2a72b5a2ab
levh <lennert.vanden.herrewegen@smals.be>
2024-01-31 10:18:15 +0100

    REFACTOR: clean up Factory

--- src/main/java/com/gildedtros/application/gildedtros/factory/GildedTrosItemFactory.java
+++ src/main/java/com/gildedtros/application/gildedtros/factory/GildedTrosItemFactory.java
@@ -5,45 +5,44 @@ import com.gildedtros.domain.item.factory.UpdatableItemFactory;
 import com.gildedtros.domain.item.model.UpdatableItem;
 import com.gildedtros.domain.item.model.implementation.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public final class GildedTrosItemFactory implements UpdatableItemFactory {
 
     private final HashMap<Predicate<String>,
-            Function<Item, UpdatableItem>> itemTypes = new HashMap<>();
+            Function<Item, UpdatableItem>> itemTypesWithChecks = new HashMap<>();
     private static final String B_DAWG_KEYCHAIN = "B-DAWG Keychain";
     private static final String GOOD_WINE = "Good Wine";
     private static final String BACKSTAGE_PASSES = "Backstage passes";
     private static final List<String> SMELLY_ITEMS = Arrays.asList("Duplicate Code", "Long Methods", "Ugly Variable Names");
 
     public GildedTrosItemFactory() {
-        itemTypes.put(isEqualTo(B_DAWG_KEYCHAIN), LegendaryItem::new);
-        itemTypes.put(isEqualTo(GOOD_WINE), IncreasingQualityItem::new);
-        itemTypes.put(startsWith(BACKSTAGE_PASSES), BackstagePassesItem::new);
-        itemTypes.put(SMELLY_ITEMS::contains, SmellyItem::new);
+        itemTypesWithChecks.put(isEqualTo(B_DAWG_KEYCHAIN), LegendaryItem::new);
+        itemTypesWithChecks.put(isEqualTo(GOOD_WINE), IncreasingQualityItem::new);
+        itemTypesWithChecks.put(startsWith(BACKSTAGE_PASSES), BackstagePassesItem::new);
+        itemTypesWithChecks.put(SMELLY_ITEMS::contains, SmellyItem::new);
     }
-
-    private Function<Item, UpdatableItem> createItem = DefaultItem::new;
 
     @Override
     public UpdatableItem createGildedTrosItem(final Item item) {
-        itemTypes.forEach(
-                (key, entry) -> {
-                    if (isNonDefaultItem(item, key)) {
-                        createItem = entry;
-                    }
-                }
-        );
-        return createItem.apply(item);
+        return getNonDefaultItem(item)
+                .orElseGet(() -> new DefaultItem(item));
     }
 
-    private static boolean isNonDefaultItem(final Item item, final Predicate<String> key) {
-        return key.test(item.name);
+    private Optional<UpdatableItem> getNonDefaultItem(final Item item) {
+        for (final Map.Entry<Predicate<String>, Function<Item, UpdatableItem>> itemTypeWithCheck : itemTypesWithChecks.entrySet()) {
+            final Predicate<String> isItemType = itemTypeWithCheck.getKey();
+            final Function<Item, UpdatableItem> itemType = itemTypeWithCheck.getValue();
+
+            if (isItemType.test(item.name)) {
+                return Optional.of(itemType.apply(item));
+            }
+        }
+        return Optional.empty();
     }
+
 
     private Predicate<String> isEqualTo(final String value) {
         return s -> s.equals(value);
